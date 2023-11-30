9f20d1ce520b1376d142e8f2164cb0408aa97551
ahmedsalah7900 <ahmed.salah7900@gmail.com>
2023-11-30 23:21:33 +0200

    create essential functions

---
+++ app.js
@@ -0,0 +1,43 @@
+const options = ["rock", "paper", "scissors"];
+
+// a function to get the computer choice
+function getComputerChoice() {
+  const choice = options[Math.floor(Math.random() * 3)];
+  console.log(choice);
+  return choice;
+}
+
+// a function to check if it's a tie, a win, or a lose
+function checkWinner(player1, computer) {
+  if (
+    (player1 == "rock" && computer == "scissors") ||
+    (player1 == "scissors" && computer == "paper") ||
+    (player1 == "paper" && computer == "rock")
+  ) {
+    return player1;
+  } else if (
+    (computer == "rock" && player1 == "scissors") ||
+    (computer == "scissors" && player1 == "paper") ||
+    (computer == "paper" && player1 == "rock")
+  ) {
+    return computer;
+  } else {
+    return "Tie!";
+  }
+}
+
+// a function to declare the winner
+function gameRound(player1, computer) {
+  const result = checkWinner(player1, computer);
+  if (result == "Tie!") {
+    return "It's a tie game!";
+  } else if (result == player1) {
+    return `U win mafaka ${player1} beats ${computer}.`;
+  } else {
+    return `U lose idiot! ${computer} beats ${player1}.`;
+  }
+}
+
+const player1 = "rock";
+const computer = getComputerChoice();
+console.log(gameRound(player1, computer));
